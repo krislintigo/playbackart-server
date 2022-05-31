e89129601a6b9ec266d5516098efdee4c12cddba
vladz <vladzholon@gmail.com>
2022-05-31 19:31:40 +0300

    Updated feature

--- src/domain/item/services/item.service.ts
+++ src/domain/item/services/item.service.ts
@@ -1,31 +1,35 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { User, UserDocument } from '../../auth/schemas/user.schema';
-import { Model } from 'mongoose';
+import { Model, Types } from 'mongoose';
 import { ItemDto } from '../dtos/item.dto';
 import { UpdateItemDto } from '../dtos/update-item.dto';
 import { answers } from '../../../constants/answers';
+import { Item } from '../schemas/item.schema';
 
 @Injectable()
 export class ItemService {
   constructor(@InjectModel(User.name) private userModel: Model<UserDocument>) {}
 
   async create(item: ItemDto, id: string): Promise<ItemDto> {
-    this.userModel.updateOne({ _id: id }, { $push: { items: { ...item } } });
+    await this.userModel.updateOne(
+      { _id: id },
+      { $push: { items: { ...item, id: new Types.ObjectId().toHexString() } } },
+    );
     return item;
   }
 
-  async findAll(userID: string) {
+  async findAll(userID: string): Promise<Item[]> {
     const user = await this.userModel.findById(userID);
     return user.items;
   }
 
-  async findByType(userID: string, type: string) {
+  async findByType(userID: string, type: string): Promise<Item[]> {
     const user = await this.userModel.findById(userID);
     return user.items.filter((item) => item.type === type);
   }
 
-  async findOne(userID: string, itemID: string) {
+  async findOne(userID: string, itemID: string): Promise<Item> {
     const user = await this.userModel.findOne(
       { _id: userID },
       {
@@ -39,15 +43,11 @@ export class ItemService {
   }
 
   async update(item: UpdateItemDto, id: string) {
-    const user = await this.userModel.findById(id);
-    const itemIndex = user.items.findIndex((i) => i.id === item.id);
-    if (itemIndex === -1) {
-      throw new NotFoundException(answers.error.item.notFound);
-    }
-    user.items[itemIndex] = {
-      ...user.items[itemIndex],
-      ...item,
-    };
-    return await user.save();
+    const updatedItem = await this.findOne(id, item.id);
+    await this.userModel.updateOne(
+      { _id: id, 'items.id': item.id },
+      { $set: { 'items.$': { ...updatedItem, ...item } } },
+    );
+    return { ...updatedItem, ...item };
   }
 }
