015402873c876b26da1f678c9a6ca37fb2153771
1sses <vladzholon@gmail.com>
2023-09-29 15:36:27 +0300

    working

--- src/services/item/item.class.ts
+++ src/services/item/item.class.ts
@@ -10,7 +10,7 @@ import { developersAggregation } from './mongodb/developers.aggregation'
 import { genresAggregation } from './mongodb/genres.aggregation'
 import { restrictionsAggregation } from './mongodb/restrictions.aggregation'
 import { ratingsAggregation } from './mongodb/ratings.aggregation'
-import { cloneDeep, values } from 'lodash'
+import { cloneDeep, includes, values } from 'lodash'
 export type { Item, ItemData, ItemPatch, ItemQuery }
 
 export interface SimpleStatistic<T> {
@@ -19,9 +19,12 @@ export interface SimpleStatistic<T> {
 }
 
 export interface ExtendedStatistic<T> extends SimpleStatistic<T> {
-  ratings: number[]
-  durations: number[]
-  fullDurations: number[]
+  coefficient: number
+  items: Array<{
+    rating: number
+    duration: number
+    fullDuration: number
+  }>
 }
 
 export interface FiltersOutput {
@@ -47,7 +50,7 @@ export class ItemService<ServiceParams extends Params = ItemParams> extends Mong
   ItemParams,
   ItemPatch
 > {
-  async filters(
+  async filters_old(
     { userId, type }: { userId: string | undefined; type?: Item['type'] },
     _params?: ServiceParams,
   ): Promise<FiltersOutput> {
@@ -86,51 +89,56 @@ export class ItemService<ServiceParams extends Params = ItemParams> extends Mong
     return result.at(0)
   }
 
-  async filters2(
+  async filters(
     { userId, type }: { userId: string | undefined; type?: Item['type'] },
     _params?: ServiceParams,
   ) {
     const items = await this.find({
       query: {
-        // ...(type && { type }),
+        ...(type && { type }),
         userId,
-        name: 'TEST',
-        // $select: [
-        //   'config',
-        //   'name',
-        //   'status',
-        //   'time',
-        //   'rating',
-        //   'restriction',
-        //   'genres',
-        //   'developers',
-        //   'franchise',
-        //   'parts',
-        // ],
-        // $limit: 20,
+        // name: 'Аврора',
       },
       paginate: false,
     })
 
-    const part = (i: Item['time'], full: boolean) => (full ? i.replays + 1 : 1) * i.count * i.duration
+    const computeDuration = (
+      item: Item,
+      { full = false, includeParts = true }: { full?: boolean; includeParts?: boolean },
+    ) => {
+      const part = (i: Item['time']) => (full ? i.replays + 1 : 1) * i.count * i.duration
 
-    // const computePartableDuration = (item: Item, full: boolean) => {
-    //   return part(item.time, full) + item.parts.reduce((acc, cur) => acc + part(cur.time, full), 0)
-    // }
-    const computeUnpartableDuration = (item: any, full: boolean) => part(item.time, full)
+      return (
+        part(item.time) + (includeParts ? item.parts?.reduce((acc, cur) => acc + part(cur.time), 0) ?? 0 : 0)
+      )
+    }
 
-    // const round = (x: number) => Math.floor(x + 0.5)
+    const ratingCoefficient = (rating: number) => {
+      switch (rating) {
+        case 10:
+          return 7
+        case 9:
+          return 2
+        default:
+          return 1
+      }
+    }
 
-    const DEFAULT_EXTENDED_STATISTICS = {
+    const DEFAULT_SIMPLE_STATISTICS = {
+      count: 0,
+    }
+
+    const DEFAULT_EXTENDED_STATISTICS: Omit<ExtendedStatistic<string>, 'value'> = {
+      coefficient: 0,
+      count: 0,
       items: [],
     }
 
-    // console.time('js')
-    const ratingsMap = new Map([1, 2, 3, 4, 5, 6, 7, 8, 9, 10].map((i) => [i, { count: 0 }]))
-    const restrictionsMap = new Map(['G', 'PG', 'PG-13', 'R', 'NC-17'].map((i) => [i, { count: 0 }]))
-    const genresMap = new Map<string, any>()
-    const developersMap = new Map<string, any>()
-    const franchisesMap = new Map<string, any>()
+    const ratingsMap = new Map<number, Omit<SimpleStatistic<number>, 'value'>>()
+    const restrictionsMap = new Map<string, Omit<SimpleStatistic<string>, 'value'>>()
+    const genresMap = new Map<string, Omit<ExtendedStatistic<string>, 'value'>>()
+    const developersMap = new Map<string, Omit<ExtendedStatistic<string>, 'value'>>()
+    const franchisesMap = new Map<string, Omit<ExtendedStatistic<string>, 'value'>>()
     const totalMap = new Map(
       ['in-process', 'planned', 'completed', 'postponed', 'abandoned'].map((i) => [
         i,
@@ -138,18 +146,21 @@ export class ItemService<ServiceParams extends Params = ItemParams> extends Mong
       ]),
     )
     for (const item of items) {
-      const fullParts = [item, ...item.parts]
+      const fullParts = [item, ...item.parts] as Item[]
       // ratings
-      const ratings = [...new Set([item.rating, ...item.parts.map((part) => part.rating)].filter(Boolean))]
+      const ratings = [...new Set(fullParts.map((part) => part.rating).filter(Boolean))]
       for (const rating of ratings) {
-        const currentRating = ratingsMap.get(rating)
+        const currentRating =
+          ratingsMap.get(rating) ?? ratingsMap.set(rating, cloneDeep(DEFAULT_SIMPLE_STATISTICS)).get(rating)
         if (currentRating) {
           currentRating.count++
         }
       }
       // restrictions
       const restriction = item.restriction
-      const currentRestriction = restrictionsMap.get(restriction)
+      const currentRestriction =
+        restrictionsMap.get(restriction) ??
+        restrictionsMap.set(restriction, cloneDeep(DEFAULT_SIMPLE_STATISTICS)).get(restriction)
       if (currentRestriction) {
         currentRestriction.count++
       }
@@ -161,19 +172,17 @@ export class ItemService<ServiceParams extends Params = ItemParams> extends Mong
         if (currentGenre) {
           for (const part of fullParts) {
             const rating = part.rating || item.rating
-            if (!rating || !part.time.duration) continue
+            if (!rating) continue
             currentGenre.items.push({
-              rating: part.rating,
-              duration: computeUnpartableDuration(part, false),
-              fullDuration: computeUnpartableDuration(part, true),
+              rating,
+              duration: computeDuration(part, {}),
+              fullDuration: computeDuration(part, { full: true }),
             })
           }
         }
       }
       // developers
-      const developers = [
-        ...new Set([...item.developers, ...item.parts.map((part) => part.developers).flat(1)]),
-      ]
+      const developers = [...new Set(fullParts.map((part) => part.developers).flat(1))]
       for (const developer of developers) {
         const currentDeveloper =
           developersMap.get(developer) ??
@@ -181,29 +190,31 @@ export class ItemService<ServiceParams extends Params = ItemParams> extends Mong
         if (currentDeveloper) {
           for (const part of fullParts) {
             const rating = part.rating || item.rating
-            if (!rating || !part.time.duration || !part.developers.includes(developer)) continue
+            if (!rating || !part.developers.includes(developer)) continue
             currentDeveloper.items.push({
               rating,
-              duration: computeUnpartableDuration(part, false),
-              fullDuration: computeUnpartableDuration(part, true),
+              duration: computeDuration(part, {}),
+              fullDuration: computeDuration(part, { full: true }),
             })
           }
         }
       }
       // franchises
-      const franchise = item.franchise
-      const currentFranchise =
-        franchisesMap.get(franchise) ??
-        franchisesMap.set(franchise, cloneDeep(DEFAULT_EXTENDED_STATISTICS)).get(franchise)
-      if (currentFranchise) {
-        for (const part of fullParts) {
-          const rating = part.rating || item.rating
-          if (!rating || !part.time.duration) continue
-          currentFranchise.items.push({
-            rating: part.rating,
-            duration: computeUnpartableDuration(part, false),
-            fullDuration: computeUnpartableDuration(part, true),
-          })
+      const franchise = item.franchise // also use franchise for huge elements? (Тьма)
+      if (franchise) {
+        const currentFranchise =
+          franchisesMap.get(franchise) ??
+          franchisesMap.set(franchise, cloneDeep(DEFAULT_EXTENDED_STATISTICS)).get(franchise)
+        if (currentFranchise) {
+          for (const part of fullParts) {
+            const rating = part.rating || item.rating
+            if (!rating) continue
+            currentFranchise.items.push({
+              rating,
+              duration: computeDuration(part, {}),
+              fullDuration: computeDuration(part, { full: true }),
+            })
+          }
         }
       }
       // total
@@ -211,33 +222,44 @@ export class ItemService<ServiceParams extends Params = ItemParams> extends Mong
         const status = part.status
         const currentStatus = totalMap.get(status)
         if (currentStatus) {
-          currentStatus.duration += computeUnpartableDuration(part, false)
-          currentStatus.fullDuration += computeUnpartableDuration(part, true)
+          currentStatus.duration += computeDuration(part, { includeParts: false })
+          currentStatus.fullDuration += computeDuration(part, { full: true, includeParts: false })
         }
       }
-
-      // const status = item.status
-      // const currentStatus = totalMap.get(status)
-      // if (currentStatus) {
-      //   currentStatus.count++
-      //   currentStatus.duration += computePartableDuration(item, false)
-      //   currentStatus.fullDuration += computePartableDuration(item, true)
-      // }
     }
 
-    const arrayFromMap = <A, B>(map: Map<A, B>) =>
-      [...map.entries()].map(([value, data]) => ({ value, ...data }))
+    const arrayFromMap = <A, B>(map: Map<A, B>, variableName = 'value') =>
+      [...map.entries()].map(([value, data]) => ({ [variableName]: value, ...data }))
 
     const ratings = arrayFromMap(ratingsMap) // done
     const restrictions = arrayFromMap(restrictionsMap) // done
     const genres = arrayFromMap(genresMap) // done
     const developers = arrayFromMap(developersMap) // done
     const franchises = arrayFromMap(franchisesMap) // done
-    const total = arrayFromMap(totalMap) // done
-    // console.timeEnd('js')
-    const response = { ratings, restrictions, genres, developers, franchises, total }
-    console.dir(JSON.stringify(response, null, 2))
-    return {}
+    const total = arrayFromMap(totalMap, 'status') // done
+
+    const totalDuration = total.reduce((acc, cur) => acc + cur.duration, 0)
+    for (const genre of genres) {
+      genre.coefficient =
+        genre.items
+          .map((i) => i.fullDuration * ratingCoefficient(i.rating))
+          .reduce((acc, cur) => acc + cur, 0) / totalDuration
+    }
+    for (const developer of developers) {
+      developer.coefficient =
+        developer.items
+          .map((i) => i.fullDuration * ratingCoefficient(i.rating))
+          .reduce((acc, cur) => acc + cur, 0) / totalDuration
+    }
+    for (const franchise of franchises) {
+      franchise.coefficient =
+        franchise.items
+          .map((i) => i.fullDuration * ratingCoefficient(i.rating))
+          .reduce((acc, cur) => acc + cur, 0) / totalDuration
+    }
+
+    // console.dir(JSON.stringify(developers, null, 2))
+    return { ratings, restrictions, genres, developers, franchises, total }
   }
 }
 
