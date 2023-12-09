0f8d72d212bfa95032765a062cde594b78c1b041
1sses <vladzholon@gmail.com>
2023-12-09 15:19:14 +0300

    return test

--- src/services/storage/storage.class.ts
+++ src/services/storage/storage.class.ts
@@ -55,7 +55,7 @@ export class StorageService<ServiceParams extends StorageParams = StorageParams>
       Bucket: this.bucket,
       Key: id,
     })
-    console.log(id, this.bucket)
+    console.log('getting', id)
     const result = await this.s3Client.send(getCommand)
     console.log(result)
     const headers = {
